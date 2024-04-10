f1af86e7bb184d211d6c628716349da836f7ceae
SilentCraftsman <ggambier72@gmail.com>
2024-04-10 13:48:43 +0200

    Teste pour avoir l'image dans la bannière de l'accueil (home)

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/Banner.css"; // Importez le fichier CSS contenant les styles
 
-function Banner({ texte }) {
+function Banner({ texte, image }) {
   return (
     <div className="banner">
-      <div className="banner__dark">Test</div>
+      <img className="banner__img" src={image} alt="Bannière" />
+      <div className="banner__dark"></div>
       <span className="banner__text">{texte}</span>
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Banner from "../components/Banner";
+import imgtest from "../images/test.jpg";
 
 function home() {
   return (
     <div>
       <h1>Page d'accueil</h1>
-      <Banner />
+      <Banner texte="Bienvenue sur notre site !" image={imgtest} />
     </div>
   );
 }
