00a46042116f370a92f126b95d71bd60b9c244e7
SilentCraftsman <ggambier72@gmail.com>
2024-04-10 14:47:05 +0200

    Création de Card.js avec pour m'aider à visualiser du CSS (temporaire et remplacé dans l'avenir par Sass)

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Card.css"; // Importez le fichier CSS contenant les styles
+
+function Card({ image, title }) {
+  return (
+    <div className="card">
+      <img src={image} alt={title} className="card__img" />
+      <div className="card__overlay"></div>
+      <h2 className="card__title">{title}</h2>
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Card from "../components/Card";
 import imgbanner from "../images/homeBanner.png";
 
 function home() {
@@ -11,6 +12,7 @@ function home() {
       <Nav />
       <h1>Page d'accueil</h1>
       <Banner texte="Bienvenue sur notre site !" image={imgbanner} />
+      <Card />
     </div>
   );
 }
