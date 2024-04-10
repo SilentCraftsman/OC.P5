eced96acd7f4e201bb56c23c525804a14d5b171e
SilentCraftsman <ggambier72@gmail.com>
2024-04-10 14:13:30 +0200

    Création du CSS d'Header pour m'aider à visualiser (temporaire et remplacé dans l'avenir par Sass)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../images/Logo/LOGO.svg";
+import "../styles/Header.css"; // Importez le fichier CSS contenant les styles
 
 function Header() {
   return (
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
-import imgtest from "../images/test.jpg";
+import imgbanner from "../images/homeBanner.png";
 
 function home() {
   return (
     <div>
       <Header />
       <h1>Page d'accueil</h1>
-      <Banner texte="Bienvenue sur notre site !" image={imgtest} />
+      <Banner texte="Bienvenue sur notre site !" image={imgbanner} />
     </div>
   );
 }
