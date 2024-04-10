0cd2aaf2cfc03841f4d1ac32f2470a5cf25af980
SilentCraftsman <ggambier72@gmail.com>
2024-04-10 20:41:51 +0200

    Creation de la page (Fiche Logement)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import Home from "./pages/home";
+import FichLogement from "./pages/fichelogement";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate replace to="/accueil" />} />
         <Route path="/accueil" element={<Home />} />
+        <Route path="/logement/:id" element={<FichLogement />} />
       </Routes>
     </HashRouter>
   );
