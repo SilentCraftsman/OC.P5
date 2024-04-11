dbd7c296c21c681c042b59395947ba579f506982
SilentCraftsman <ggambier72@gmail.com>
2024-04-11 21:28:33 +0200

    Création du contenu JS (JSX) de ErrorP.js mais pas encore de route créé

--- src/pages/ErrorP.js
+++ src/pages/ErrorP.js
@@ -1,3 +1,20 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Error from "../components/ErrorC";
+
+const NoPage = () => {
+  return (
+    <>
+      <div className="NoPage">
+        <Header />
+        <main>
+          <Error />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default NoPage;
