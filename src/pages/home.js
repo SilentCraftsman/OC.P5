8b89d78840766cd8188565a17de827cbae632326
SilentCraftsman <ggambier72@gmail.com>
2024-04-16 14:46:56 +0200

    Création et test des premières variables en SASS

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import imgbanner from "../images/homeBanner.png";
 import Gallery from "../components/Gallery";
 import Footer from "../components/Footer";
+import "../styles/Saas tools/sty.scss";
 
 function home() {
   return (
