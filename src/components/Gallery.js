a019b18682e545dd582dc49ef3317498850cfaa4
SilentCraftsman <ggambier72@gmail.com>
2024-04-16 17:17:44 +0200

    Création du style de la Gallery en SCSS

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logements from "../data.json";
 import Card from "../components/Card";
-import "../styles/Gallery.css"; // Importez le fichier CSS contenant les styles
+import "../styles/Gallery.scss"; // Importez le fichier CSS contenant les styles
 
 const Gallery = () => {
   return (
